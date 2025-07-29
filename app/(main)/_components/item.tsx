1d2af7fa89518dfd05286530f25144640fde8c24
Vitaliy Boyanivskyy <vitalik.boyan@gmail.com>
2025-07-29 07:26:54 +0200

    feat: add archiving functionality

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,11 +1,26 @@
 "use client";
 
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/nextjs";
 import { useMutation } from "convex/react";
-import { ChevronDown, ChevronUp, LucideIcon, Plus } from "lucide-react";
+import {
+	ChevronDown,
+	ChevronUp,
+	LucideIcon,
+	MoreHorizontal,
+	Plus,
+	Trash,
+} from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
@@ -34,7 +49,9 @@ export const Item = ({
 	icon: Icon,
 	onClick,
 }: ItemProps) => {
+	const { user } = useUser();
 	const create = useMutation(api.documents.create);
+	const archive = useMutation(api.documents.archive);
 	const router = useRouter();
 
 	const handleExpand = (
@@ -67,6 +84,24 @@ export const Item = ({
 			loading: "Creating a new node...",
 			success: "New node created!",
 			error: "Failed to create node.",
+		});
+	};
+
+	const onArchieve = (
+		event: React.MouseEvent<HTMLDivElement, MouseEvent>
+	) => {
+		event.stopPropagation();
+
+		if (!id) return;
+
+		const promise = archive({
+			id,
+		});
+
+		toast.promise(promise, {
+			loading: "Moving node to trash...",
+			success: "Node moved to trash",
+			error: "Failed to archieve node",
 		});
 	};
 
@@ -111,6 +146,35 @@ export const Item = ({
 			)}
 			{!!id && (
 				<div className="ml-auto flex items-center gap-x-2">
+					<DropdownMenu>
+						<DropdownMenuTrigger
+							asChild
+							onClick={(e) => e.stopPropagation()}
+						>
+							<div
+								role="buttun"
+								className="cursor-pointer p-0.5 opacity-0 group-hover:opacity-100 
+								h-full ml-auto rounded-lg hover:bg-neutral-300 dark:hover:bg-neutral-600"
+							>
+								<MoreHorizontal className="h-4 w-4 text-muted-foreground" />
+							</div>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent
+							className="w-60"
+							align="start"
+							side="right"
+							forceMount
+						>
+							<DropdownMenuItem onClick={onArchieve}>
+								<Trash className="h-4 w-4 mr-2" />
+								Delete
+							</DropdownMenuItem>
+							<DropdownMenuSeparator />
+							<div className="text-xs text-muted-foreground p-2">
+								Last Edited By: {user?.fullName}
+							</div>
+						</DropdownMenuContent>
+					</DropdownMenu>
 					<div
 						role="button"
 						className="cursor-pointer opacity-0 group-hover:opacity-100 h-full 
--- convex/documents.ts
+++ convex/documents.ts
@@ -54,6 +54,53 @@ export const getSidebar = query({
 	},
 });
 
+export const archive = mutation({
+	args: { id: v.id("documents") },
+	handler: async (context, args) => {
+		const identity = await context.auth.getUserIdentity();
+
+		if (!identity) {
+			throw new Error("Not authenticated");
+		}
+
+		const userId = identity.subject;
+
+		const existingDocument = await context.db.get(args.id);
+
+		if (!existingDocument) {
+			throw new Error("Not found");
+		}
+
+		if (existingDocument.userId !== userId) {
+			throw new Error("Unauthorized");
+		}
+
+		const recursiveArchive = async (documentId: Id<"documents">) => {
+			const children = await context.db
+				.query("documents")
+				.withIndex("by_user_parent", (q) =>
+					q.eq("userId", userId).eq("parentDocument", documentId)
+				)
+				.collect();
+
+			for (const child of children) {
+				await context.db.patch(child._id, {
+					isArchived: true,
+				});
+				await recursiveArchive(child._id);
+			}
+		};
+
+		const document = await context.db.patch(args.id, {
+			isArchived: true,
+		});
+
+		recursiveArchive(args.id);
+
+		return document;
+	},
+});
+
 // export const get = query({
 // 	args: {
 // 		userId: v.optional(v.string()),
